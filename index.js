68a51c6119adc33c4ea2f2148a9e668880d33b8b
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-07-13 13:18:08 +0100

    Create the basic functions
    Make provision for add, subtract, multiply, and divide

---
+++ index.js
@@ -0,0 +1,17 @@
+// Operations
+
+const add = (x, y) => {
+  return x + y
+}
+
+const multiply = (x, y) => {
+  return x * y
+}
+
+const divide = (x, y) => {
+  return x/y
+}
+
+const subtract = (x, y) => {
+  return x - y
+}
