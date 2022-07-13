f5a6e6ba96e3c6c91c8532fa92ab23b1fe6e8d73
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-07-13 13:25:12 +0100

    Create an operate function as specified in rubric

--- index.js
+++ index.js
@@ -15,3 +15,23 @@ const divide = (x, y) => {
 const subtract = (x, y) => {
   return x - y
 }
+
+//Operate function
+const operate = (operand1, operator, operand2) => {
+  switch (operator) {
+    case '+':
+      return add(operand1, operand2)
+      break;
+    case '-':
+      return subtract(operand1, operand2)
+      break;
+    case '/':
+      return divide(operand1, operand2)
+      break;
+    case '*':
+      return multiply(operand1, operand2)
+      break;  
+    default:
+      break;
+  }
+}
